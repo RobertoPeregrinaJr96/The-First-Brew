0f3b50d7c21ef2ee5be8fae5834a4d5147799ce8
Roberto Peregrina <111211515+RobertoPeregrinaJr96@users.noreply.github.com>
2023-06-20 08:22:23 -0700

    Update index.js

--- frontend/src/components/ShoppingCart/index.js
+++ frontend/src/components/ShoppingCart/index.js
@@ -93,7 +93,7 @@ const ShoppingCart = () => {
             <h1>Shopping Cart</h1>
 
             <ul className="cart-div-ul">
-                {sortedItems?.sort((a, b) => a.id + b.ib)?.map(item => {
+                {sortedItems?.sort((a, b) => a.id + b.id) && sortedItems?.sort((a, b) => a.id + b.id)?.map(item => {
                     const coffee = item?.Coffee
                     return <li key={item?.id} className="cart-div-li">
 
