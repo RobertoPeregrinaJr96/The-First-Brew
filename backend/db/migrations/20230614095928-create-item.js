a2a404fc9fda32ec444164916cf9bbcf388e0b09
RobertoPeregrinaJr <robertoperegrina1996@gmail.com>
2024-02-12 06:07:22 -0800

    updated migration Order item

---
+++ backend/db/migrations/20230614095928-create-item.js
@@ -0,0 +1,57 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Items', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      price: {
+        type: Sequelize.DECIMAL(10, 2),
+        allowNull: false,
+      },
+      description: {
+        type: Sequelize.TEXT,
+        allowNull: false,
+      },
+      defaultModifiers:{
+        type:Sequelize.TEXT,
+        allowNull: false,
+      },
+      itemImg:{
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      waitTiem:{
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+      }
+    }, options);
+  },
+  async down(queryInterface, Sequelize) {
+    options.tableName = "Items";
+    await queryInterface.dropTable(options);
+  }
+};
