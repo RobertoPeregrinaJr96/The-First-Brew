6e262c95c93847d4ab61c137b5b4ec1f5d576fe2
RobertoPeregrinaJr <robertoperegrina1996@gmail.com>
2024-02-12 06:27:07 -0800

    renamed migration file

---
+++ backend/db/migrations/20230726005909-create-instrunctions-modifier.js
@@ -0,0 +1,38 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA
+}
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('InstructionMondifiers', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      itemId: {
+        type: Sequelize.INTEGER
+      },
+      modifierId: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+      }
+    }, options);
+  },
+  async down(queryInterface, Sequelize) {
+    options.tableName = 'InstructionMondifiers'
+    await queryInterface.dropTable(options);
+  }
+};
