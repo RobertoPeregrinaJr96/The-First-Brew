9ae6d21d9fcf27a4fc18ce68cd2c56513c28f601
RobertoPeregrinaJr <robertoperegrina1996@gmail.com>
2024-02-12 06:08:51 -0800

    Update 20230614095513-create-order-item.js

--- backend/db/migrations/20230614095513-create-order-item.js
+++ backend/db/migrations/20230614095513-create-order-item.js
@@ -1,59 +1,59 @@
-'use strict';
+"use strict";
 /** @type {import('sequelize-cli').Migration} */
 let options = {};
-if (process.env.NODE_ENV === 'production') {
-  options.schema = process.env.SCHEMA;  // define your schema in options object
+if (process.env.NODE_ENV === "production") {
+  options.schema = process.env.SCHEMA; // define your schema in options object
 }
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Items', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      cartId: {
-        type: Sequelize.INTEGER,
-        reference: {
-          model: "ShoppingCarts",
-          key: 'id'
+    await queryInterface.createTable(
+      "OrderItems",
+      {
+        id: {
+          allowNull: false,
+          autoIncrement: true,
+          primaryKey: true,
+          type: Sequelize.INTEGER,
         },
-        allowNull: false,
-      },
-      coffeeId: {
-        type: Sequelize.INTEGER,
-        reference: {
-          model: 'Items',
-          key: 'id'
+        orderId: {
+          type: Sequelize.INTEGER,
+          reference: {
+            model: "Orders",
+            key: "id",
+          },
+          allowNull: false,
         },
-        allowNull: false,
-      },
-      instructionId: {
-        type: Sequelize.INTEGER,
-        reference: {
-          model: 'Instructions',
-          key: 'id'
+        itemId: {
+          type: Sequelize.INTEGER,
+          reference: {
+            model: "Items",
+            key: "id",
+          },
+          allowNull: false,
+        },
+        customInstructions: {
+          type: Sequelize.Text,
+        },
+        quantity: {
+          type: Sequelize.INTEGER,
+          allowNull: false,
+        },
+        createdAt: {
+          allowNull: false,
+          type: Sequelize.DATE,
+          defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+        },
+        updatedAt: {
+          allowNull: false,
+          type: Sequelize.DATE,
+          defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
         },
       },
-      quantity: {
-        type: Sequelize.INTEGER,
-        allowNull: false
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE,
-        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE,
-        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
-      }
-    }, options);
+      options
+    );
   },
   async down(queryInterface, Sequelize) {
-    options.tableName = "Items";
+    options.tableName = "OrderItems";
     await queryInterface.dropTable(options);
-  }
+  },
 };
