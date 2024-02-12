f1bf622223c0d3ba896d430ad83b7ec2ec2e98ec
RobertoPeregrinaJr <robertoperegrina1996@gmail.com>
2024-02-12 06:21:39 -0800

    Update 20230708224555-create-analytics.js

--- backend/db/migrations/20230708224555-create-analytics.js
+++ backend/db/migrations/20230708224555-create-analytics.js
@@ -1,39 +1,58 @@
-'use strict';
+"use strict";
 /** @type {import('sequelize-cli').Migration} */
 let options = {};
-if (process.env.NODE_ENV === 'production') {
-  options.schema = process.env.SCHEMA;  // define your schema in options object
+if (process.env.NODE_ENV === "production") {
+  options.schema = process.env.SCHEMA; // define your schema in options object
 }
 
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Analytics', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
+    await queryInterface.createTable(
+      "Analytics",
+      {
+        id: {
+          allowNull: false,
+          autoIncrement: true,
+          primaryKey: true,
+          type: Sequelize.INTEGER,
+        },
+        userId: {
+          type: Sequelize.INTEGER,
+        },
+        totatlOrders: {
+          type: Sequelize.INTEGER,
+        },
+        totatlSpent: {
+          type: Sequelize.INTEGER,
+        },
+        totatlOrders: {
+          type: Sequelize.INTEGER,
+        },
+        avgOrderValue: {
+          type: Sequelize.INTEGER,
+        },
+        orderFrequency: {
+          type: Sequelize.INTEGER,
+        },
+        lastPurchase: {
+          type: Sequelize.DATE,
+        },
+        createdAt: {
+          allowNull: false,
+          type: Sequelize.DATE,
+          defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+        },
+        updatedAt: {
+          allowNull: false,
+          type: Sequelize.DATE,
+          defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+        },
       },
-      userId: {
-        type: Sequelize.INTEGER
-      },
-      purchase_historyId: {
-        type: Sequelize.INTEGER
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE,
-        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE,
-        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
-      }
-    }, options);
+      options
+    );
   },
   async down(queryInterface, Sequelize) {
-    options.tableName = "Analytics"
+    options.tableName = "Analytics";
     await queryInterface.dropTable(options);
-  }
+  },
 };
