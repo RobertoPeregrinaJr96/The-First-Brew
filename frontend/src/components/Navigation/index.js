09c2fbb50f8fe47e73509b7f002251e4cb400e90
RobertoPeregrinaJr <robertoperegrina1996@gmail.com>
2024-04-24 16:24:47 -0700

    image link update

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -18,7 +18,7 @@ function Navigation({ isLoaded }) {
     <div className='nav-wrapper'>
       <div className='nav-div-topLeft'>
         <NavLink exact to="/">
-          <img className='nav-list-img' src='https://cdn.discordapp.com/attachments/1088906268485357618/1118284501710295100/PngItem_30012.png' alt=''></img>
+          <img className='nav-list-img' src='https://cdn.discordapp.com/attachments/1088906268485357618/1232832890530955274/cup_of_coffee.png?ex=662ae49a&is=6629931a&hm=af82cde615cd733d5b5ec2ccd6bbf41c673f5c6ce5862679426e89bc83eafcd3&' alt=''></img>
         </NavLink>
         <NavLink exact to="/">
           <h1 className='nav-h1'>The First Brew</h1>
